340074ef9dc26456bfdebf2f847b02c16bda0f33
Daniel Rodríguez Escobar <drodr@bitergia.com>
2023-02-24 12:43:41 +0000

    [terraform] Remove public access to SortingHat GCS bucket
    The commit removes the public access to SortingHat bucket
    in order to have better security.
    
    Signed-off-by: Daniel Rodríguez Escobar <drodr@bitergia.com>

--- terraform/modules/bap_env_gcp/storage.tf
+++ terraform/modules/bap_env_gcp/storage.tf
@@ -7,7 +7,7 @@ resource "google_storage_bucket" "backups_assets" {
   name         = random_id.backups_assets_bucket_id.hex
   location     = var.backups_storage_location
 
-  uniform_bucket_level_access = false
+  uniform_bucket_level_access = var.uniform_bucket_level_access
   force_destroy = true
 }
 
@@ -20,18 +20,7 @@ resource "google_storage_bucket" "sortinghat_assets" {
   name         = random_id.sortinghat_assets_bucket_id.hex
   location     = var.sortinghat_storage_location
 
-  uniform_bucket_level_access = false
+  uniform_bucket_level_access = var.uniform_bucket_level_access
   force_destroy = true
 }
 
-resource "google_storage_bucket_access_control" "public_rule" {
-  bucket = google_storage_bucket.sortinghat_assets.name
-  role   = "READER"
-  entity = "allUsers"
-}
-
-resource "google_storage_bucket_iam_binding" "binding_allUsers" {
-  bucket = google_storage_bucket.sortinghat_assets.name
-  role = "roles/storage.objectViewer"
-  members = ["allUsers"]
-}
